da4d7dce0ae3f2863de6a9b1ab3a066ad344ed03
PLAYZHACKING <playzhacking12946@gmail.com>
2025-04-06 19:46:05 +0530

    start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -61,9 +61,9 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("❣️ Developer", url = "https://t.me/PLAYZ_90")
+        InlineKeyboardButton("👑 Developer", url = "https://t.me/PLAYZ_90")
     ],[
-        InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/vj_bot_disscussion'),
+        InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/PLAY_Z_HACKING_DISCUSSION'),
         InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/+ahE-9i84aFxkOWNl')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
