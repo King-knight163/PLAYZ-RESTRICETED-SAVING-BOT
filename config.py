470dbb65d58060e1dae96fc74777e1327a8055d2
play-z-hostinger <crunchfun885@gmail.com>
2025-09-25 08:22:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8142366006:AAG6miPwohzaYwVyMW9rwdPnauHSQ1uxIPk")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8142366006:AAGMRm166OnRDQBY-VBhyhyg1fyzPnrrWcY")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "22161204"))
